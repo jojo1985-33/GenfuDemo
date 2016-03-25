f4065d35338e920d4440911d18cd71594d3e3b8b
Stefan Joachimsthaler <stefanjoachimsthaler@gmail.com>
2016-03-25 20:32:25 +0100

    initial commit

---
+++ GenfuDemo/Models/CustomerService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using GenFu;
+
+namespace GenfuDemo.Models
+{
+    class CustomerService : ICustomerService
+    {
+        private List<Customer> _customers;
+
+        public CustomerService()
+        {
+            _customers = A.ListOf<Customer>();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return _customers;
+        }
+    }
+}
